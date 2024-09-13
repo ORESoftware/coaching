32db5476d9ff7a605d61cc392850f56bba254b98
Alex.Mills <amills@vibeirl.com>
2024-09-13 13:26:01 -0500

    chore: tmp

--- scripts/git/push.sh
+++ scripts/git/push.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 set -eo pipefail
-git add -A
+git add -A || {
+  echo 'Could not add files. Something is off. Exiting.';
+  exit 1;
+}
 
 # Check if a commit message is provided
 if [ -z "$1" ]; then
@@ -11,7 +14,14 @@ else
   COMMIT_MSG="$1"
 fi
 
-git commit -am "${COMMIT_MSG}"
+echo 'Attempting to create a new commit...';
+
+git commit -am "${COMMIT_MSG}" && echo 'Created new commit' || {
+  echo 'Could not create a new commit.'
+}
+
+echo 'Attempting to push whatever is not already on remote...';
+
 git push || {
   echo 'could not push'
 }
