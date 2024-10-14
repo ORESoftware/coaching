fe776d3441bb60ff74853a8dc718de3b76f6b61d
Alex.Mills <amills@vibeirl.com>
2024-10-14 12:54:00 -0500

    chore: tmp

---
+++ assets/coach-risk.js
@@ -0,0 +1,74 @@
+exports.coaches = {
+  "coaches": [
+    {
+      "firstName": "David",
+      "lastName": "J.",
+      "attendance": 1  //
+    },
+    {
+      "firstName": "Cory",
+      "lastName": "D.",
+      "attendance": 1  // because he has contributed a lot outside of meetings, but show up to meetings!
+    },
+    {
+      "firstName": "Ted",
+      "lastName": "S.",
+      "attendance": 1  // show up again Ted lol
+    },
+    {
+      "firstName": "Kari",
+      "lastName": "T.",
+      "attendance": 4  // the best
+    },
+    {
+      "firstName": "Alex",
+      "lastName": "M.",
+      "attendance": Infinity // king
+    },
+    {
+      "firstName": "Gidge",
+      "lastName": "B.",
+      "attendance": 4  // very good Boston Queen
+    },
+    {
+      "firstName": "Kevin",
+      "lastName": "L.",
+      "attendance": 3  // Kevin gets good marks
+    },
+    {
+      "firstName": "Elijah",
+      "lastName": "G.",
+      "attendance": 2 // good work with the new baby
+    },
+    {
+      "firstName": "Tim",
+      "lastName": "M.",
+      "attendance": 2  // Tim is hanging on
+    },
+    {
+      "firstName": "Franklin",
+      "lastName": "V.",
+      "attendance": 2  // thanks Franklin for just joining and staying onboard
+    },
+    {
+      "firstName": "Aaron",
+      "lastName": "F.",
+      "attendance": 1  // Aaron is busy but remains interested
+    },
+    {
+      "firstName": "Tyler",
+      "lastName": "K.",
+      "attendance": 1 // brand new and thanks for taking part
+    },
+    {
+      "firstName": "David",
+      "lastName": "R.",
+      "attendance": 0 // will be joining soon!
+    },
+    {
+      "firstName": "Corey",
+      "lastName": "M.",
+      "attendance": 0 // corey is busy but he wants a piece soon
+    }
+  ]
+}
